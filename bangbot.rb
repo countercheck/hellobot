ed876ed8d1553eb745bc6344ab50ceefc6b07b49
Michael Atlin <mike.atlin@gmail.com>
2014-01-23 09:32:16 -0500

    all my bots

--- bangbot.rb
+++ bangbot.rb
@@ -2,7 +2,7 @@ require "socket"
 
 server = "irc.bitmakerlabs.com"
 port = "6667"
-nick = "HelloBot"
+nick = "BangBot"
 channel = "#cohort5"
 greeting_prefix = "privmsg #{channel} :"
 greetings = ["hello", "hi", "hola", "yo", "wazup", "guten tag", "howdy", "salutations", "who the hell are you?"]
@@ -12,20 +12,19 @@ irc_server = TCPSocket.open(server, port)
 irc_server.puts "USER bhellobot 0 * BHelloBot"
 irc_server.puts "NICK #{nick}"
 irc_server.puts "JOIN #{channel}"
-irc_server.puts "PRIVMSG #{channel} :Hello from IRC Bot"
+irc_server.puts "PRIVMSG #{channel} Bang"
 
-# Hello, Bot!
+# Bang bot replies with Bang every time someone sends a message with an exclaimation mark
 until irc_server.eof? do
   msg = irc_server.gets.downcase
   puts msg
 
-  was_greeted = false
-  greetings.each do |g|
-    was_greeted = true if msg.include?(g)
-  end
+  exclaim = msg.split(//).reject { |c| c != "!" }
 
-  if msg.include?(greeting_prefix) and was_greeted
-  	response = "w00t! Someone talked to us!!!! Hello!!!"
+  if exclaim.length > 1
+    response = "BANG"
   	irc_server.puts "PRIVMSG #{channel} :#{response}"
   end
 end
+
+
--- stealthbot.rb
+++ stealthbot.rb
@@ -2,7 +2,7 @@ require "socket"
 
 server = "irc.bitmakerlabs.com"
 port = "6667"
-nick = "HelloBot"
+nick = "StealthBot"
 channel = "#cohort5"
 greeting_prefix = "privmsg #{channel} :"
 greetings = ["hello", "hi", "hola", "yo", "wazup", "guten tag", "howdy", "salutations", "who the hell are you?"]
@@ -12,20 +12,21 @@ irc_server = TCPSocket.open(server, port)
 irc_server.puts "USER bhellobot 0 * BHelloBot"
 irc_server.puts "NICK #{nick}"
 irc_server.puts "JOIN #{channel}"
-irc_server.puts "PRIVMSG #{channel} :Hello from IRC Bot"
+irc_server.puts "PRIVMSG #{channel} I'm not here"
 
-# Hello, Bot!
+# StealthBot detects whenever anyone logs off the channel, and assumes their nickname
 until irc_server.eof? do
   msg = irc_server.gets.downcase
   puts msg
 
-  was_greeted = false
-  greetings.each do |g|
-    was_greeted = true if msg.include?(g)
-  end
+  leave = false
+  leave = true if msg.include?("quit :")
 
-  if msg.include?(greeting_prefix) and was_greeted
-  	response = "w00t! Someone talked to us!!!! Hello!!!"
-  	irc_server.puts "PRIVMSG #{channel} :#{response}"
+  if leave
+    split_msg = msg.split("!")
+    new_name = split_msg[0][1..-1]
+    irc_server.puts "NICK #{new_name}"
+    response = "I just stole #{new_name}'s name!"
+    irc_server.puts "PRIVMSG #{channel} :#{response}"
   end
 end
